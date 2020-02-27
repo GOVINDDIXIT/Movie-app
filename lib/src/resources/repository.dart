8c5fb8c56998c9b97632b309c413a5737ad982af
GOVINDDIXIT <govinddixit93@GMAIL.com>
2020-02-27 23:04:30 +0530

    update feat: add movie detail

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'package:movie_app/src/models/trailer_model.dart';
+
 import 'movie_api_provider.dart';
 import '../models/item_model.dart';
 
@@ -6,4 +8,6 @@ class Repository {
   final moviesApiProvider = MovieApiProvider();
 
   Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+
+  Future<TrailerModel> fetchTrailers(int movieId) => moviesApiProvider.fetchTrailer(movieId);
 }
